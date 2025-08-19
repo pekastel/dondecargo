33bd52595f2a93d024ae93a3b6864e8e395f5153
Pekastel <pablo@lumile.com.ar>
2025-08-19 00:02:15 -0300

    chore: update HTML lang attribute from 'en' to 'es'

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="es">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<ThemeProvider>
 					<div className="min-h-screen flex flex-col">
